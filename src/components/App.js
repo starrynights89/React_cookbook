4c7bf55740e76c9a18b0d1ba56398ce0d4a9a284
Alexander Hartson <alexander-hartson@live.com>
2019-08-13 12:51:06 -0500

    Adding Content and Footers

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from '../shared/images/logo.svg';
-// Import home component
 import Home from './Home/Home';
+// Layout Components
+import Header from '../shared/components/layout/Header';
+import Content from '../shared/components/layout/Content';
+import Footer from '../shared/components/layout/Footer';
+
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/* Add Home component to be rendered */}
-        <Home/>
+        <Header title="Welcome to Codejobs" />
+        <Content>
+          <Home />
+        </Content>
+        <Footer />
       </div>
-    );  
+    );
   }
 }
 
---
+++ src/shared/components/layout/Content.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Content extends Component {
+
+  static propTypes = {
+    children: PropTypes.element.isRequired
+  };
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <main>
+        {children}
+      </main>
+    );
+  }
+}
+
+export default Content;
